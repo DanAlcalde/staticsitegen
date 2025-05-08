07c650ca0d360b5975246aee3d530141d0023c3b
DanAlcalde <danalcalde@gmail.com>
2025-05-08 16:27:16 -0400

    refactor: move copy to sep function

--- src/copy_files_recursive.py
+++ src/copy_files_recursive.py
@@ -1,16 +1,17 @@
 import os
 import shutil
 
-
-def copy_files_recursive(source_dir_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-
-    for filename in os.listdir(source_dir_path):
-        from_path = os.path.join(source_dir_path, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        print(f" * {from_path} -> {dest_path}")
-        if os.path.isfile(from_path):
-            shutil.copy(from_path, dest_path)
-        else:
-            copy_files_recursive(from_path, dest_path)
+def copy_from_dir(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    file_list = os.listdir(source)
+    for file in file_list:
+        source_path = os.path.join(source, file)
+        dest_path = os.path.join(destination, file)
+        if os.path.isfile(os.path.join(source, file)):
+            print(f"Copying {source_path} to {dest_path}")
+            shutil.copy(source_path, dest_path)  
+        elif os.path.isdir(os.path.join(source, file)):
+            print(f"Copying directory {source_path} to {dest_path}")
+            copy_from_dir(source_path, dest_path)
+    return
--- src/main.py
+++ src/main.py
@@ -1,33 +1,28 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
 from generate import generate_page, generate_pages_recursive
-from copy_files_recursive import copy_files_recursive
+from copy_files_recursive import copy_from_dir
 
+import sys
 import shutil
 import os
 
-def copy_from_dir(source, destination):
-    if not os.path.exists(destination):
-        os.mkdir(destination)
-    file_list = os.listdir(source)
-    for file in file_list:
-        source_path = os.path.join(source, file)
-        dest_path = os.path.join(destination, file)
-        if os.path.isfile(os.path.join(source, file)):
-            print(f"Copying {source_path} to {dest_path}")
-            shutil.copy(source_path, dest_path)  
-        elif os.path.isdir(os.path.join(source, file)):
-            print(f"Copying directory {source_path} to {dest_path}")
-            copy_from_dir(source_path, dest_path)
-    return
+if sys.argv[1]:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
+static_path = os.path.join(basepath, "static")
+content_path = os.path.join(basepath, "content")
+template_path = os.path.join(basepath, "template")
+public_path = os.path.join(basepath, "public")
+
+
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
-    copy_from_dir("static", "public")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
+    copy_from_dir(static_path, public_path)
     
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive(content_path, template_path, public_path)
 
 if __name__ == "__main__":
     main()
